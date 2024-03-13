2587377429c7ed630894e3095aae7a9b1c1fdc88
Thiago Schimitz <thiago.schimitz@softbyte.local>
2024-03-13 17:51:34 -0300

    Criando estrutura de pedido

---
+++ WebApplication1/Models/Pedido.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication1.Models;
+
+namespace NetRestaurantAPI.Models
+{
+    public class Pedido
+    {
+        public Guid Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime DataInclusao { get; set; }
+        public List<Product> Produtos { get; set; }
+        public float PrecoTotal { get; set; }                
+    }
+}
--- WebApplication1/Models/Usuario.cs
+++ WebApplication1/Models/Usuario.cs
@@ -6,21 +6,29 @@ namespace NetRestaurantAPI.Models
     {        
         public Usuario()
         {
+            Id = Guid.Empty;
             Email = string.Empty;
             Nome = string.Empty;
             Cpf = string.Empty;            
             Password = string.Empty;
-            Endereco = string.Empty;
+            Rua = string.Empty;
+            Bairro = string.Empty;
+            Cidade = string.Empty;
+            Numero = string.Empty;
+            Complemento = string.Empty;
         }
         public Guid Id { get; set; }
         public string Email { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
         public string Password { get; set; }
-        public string Endereco { get; set; }
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
         public bool Autenticado { get; set; }
         public string Telefone { get; set; }
-        public byte[] FotoPerfil { get;set; }
         public void EntidadeValida()
         {
             if(Id == Guid.Empty) 
@@ -35,22 +43,22 @@ namespace NetRestaurantAPI.Models
 
             if (string.IsNullOrEmpty(Nome))
             {
-                throw new Exception("Informe um email!");
+                throw new Exception("Informe um Nome!");
             }
 
             if (string.IsNullOrEmpty(Cpf))
             {
-                throw new Exception("Informe um email!");
+                throw new Exception("Informe um Cpf!");
             }
 
             if (string.IsNullOrEmpty(Password))
             {
-                throw new Exception("Informe um email!");
+                throw new Exception("Informe uma Senha!");
             }
 
-            if (string.IsNullOrEmpty(Endereco))
+            if (string.IsNullOrEmpty(Rua) || string.IsNullOrEmpty(Bairro) || string.IsNullOrEmpty(Numero) || string.IsNullOrEmpty(Cidade))
             {
-                throw new Exception("Informe um email!");
+                throw new Exception("Informe um enedereço valido!");
             }
 
             if (string.IsNullOrEmpty(Telefone) || this.Telefone.Length < 9)
